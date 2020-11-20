6b771559adcc04fc55d3c1da013132432c4acff8
Faisal Riyaz <faisalriyaz011@gmail.com>
2020-11-20 08:38:47 +0530

    Implemented an AST based checker to raise issues if a function/method has been decorated with @wraps decorator from the `functools` standard library. Added testscript and readme.

---
+++ analyzer.py
@@ -0,0 +1,39 @@
+import ast
+import sys
+import tokenize
+
+def read_file(filename):
+    with tokenize.open(filename) as fd:
+        return fd.read()
+
+class BaseChecker(ast.NodeVisitor):
+    def __init__(self):
+        self.violations = []
+
+    def check(self, paths):
+        for filepath in paths:
+            self.filename = filepath
+            tree = ast.parse(read_file(filepath))
+            self.visit(tree)
+
+    def report(self):
+        for violation in self.violations:
+            filename, lineno, msg = violation
+            print(filename + ":" + str(lineno) + ": " + msg)
+            
+class WrapDecoratorChecker(BaseChecker):
+    msg = "@wraps decorator used"
+
+    def visit_FunctionDef(self, node):
+        for decorator in node.decorator_list:
+            if isinstance(decorator, ast.Call) and decorator.func.id == "wraps":
+                self.violations.append((self.filename, node.lineno, self.msg))  
+      
+        for child in node.body:
+            self.visit(child)
+            
+if __name__ == '__main__':
+    files = sys.argv[1:]
+    checker = WrapDecoratorChecker()
+    checker.check(files)
+    checker.report()
---
+++ test.py
@@ -0,0 +1,52 @@
+from functools import wraps
+
+def add(a, b):
+    return a + b
+
+def print_function_name(func):
+    @wraps(func)
+    def logging(*args, **kwargs):
+        print(func.__name__ + " was called")
+        return func(*args, **kwargs)
+    return logging
+    
+@print_function_name
+def square(x):
+   return x * x
+            
+def nested_function(a, b, c):
+    def print_function_name(func):
+        @wraps(func)
+        def logging(*args, **kwargs):
+            print(func.__name__ + " was called")
+            return func(*args, **kwargs)
+        return logging
+    
+    @print_function_name
+    def multiply(a, b):
+        return a*b
+        
+    return multiply(a/2, b*4) + c
+    
+class MyClass:
+    def print_function_name(func):
+        @wraps(func)
+        def logging(*args, **kwargs):
+            print(func.__name__ + " was called")
+            return func(*args, **kwargs)
+        return logging
+        
+    def divide(self, a, b):
+        return a/b
+        
+    @print_function_name
+    def power(self, a, b):
+        return a**b
+        
+if __name__ == '__main__':
+    print(square(4))
+    print(add(2, 3))
+    print(nested_function(2, 3, 4))
+    obj = MyClass()
+    print(obj.divide(10, 2))
+    print(obj.power(2, 4))
