95a85f645a1706d0947e5f0a561581f36f01d1e9
Srijan Saurav <sauravsrijan@gmail.com>
2020-11-23 15:29:03 +0530

    Add more test cases

---
+++ more_tests.py
@@ -0,0 +1,24 @@
+import functools
+
+def print_function_name(func):
+    @funcrools.wraps(func)  # An issue should be raised here
+    def logging(*args, **kwargs):
+        print(func.__name__ + " was called")
+        return func(*args, **kwargs)
+    return logging
+
+
+def wraps(func):
+    def wrap(*args, **kwargs):
+        print("Not functool's wrap")
+        return func(*args, **kwargs)
+
+    return wrap
+
+
+@wraps  # No issue here. This is fine.
+def foo(x):
+    print(x)
+
+
+foo("DeepSource")
